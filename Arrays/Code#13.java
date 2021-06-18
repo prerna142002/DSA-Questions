0005dabd18285a7786ed4dd6f777788cc33eed3f
Prerna Verma <55102903+prerna142002@users.noreply.github.com>
2021-06-18 20:11:18 +0530

    Kadane Algorithm
    Kadane Algorithm

---
+++ Arrays/Code#13.java
@@ -0,0 +1,18 @@
+class Solution{
+
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+
+        // Your code here
+        int max = Integer.MIN_VALUE,current=0;
+        for(int i: arr){
+            current += i;
+            if(current>=max) max = current;
+            if(current<0) current=0;
+        }
+        return max;
+    }
+
+}
